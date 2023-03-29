b370413739e4230f181175153aa65db1ac411e1b
Darkhto <aimannazri020@gmail.com>
2023-03-29 13:42:12 +0800

    added topic handler

---
+++ app/src/main/java/com/example/TopicHandler/main.java
@@ -0,0 +1,13 @@
+package com.example.TopicHandler;
+
+public class main {
+
+    public static void main(String[] args) {
+
+        TopicHandler con = new TopicHandler();
+        //con.connector();
+        //con.save();
+
+        con.GetPost(1);
+    }
+}
