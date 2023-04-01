1a7e7b7c7d05d0636b8fe4536ed72c998fb80f28
sofeadayana <sofeadayana26@gmail.com>
2023-04-01 17:42:36 +0800

    part amira 2

--- app/src/main/java/com/example/registration/HomepageActivity.java
+++ app/src/main/java/com/example/registration/HomepageActivity.java
@@ -2,12 +2,36 @@ package com.example.registration;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class HomepageActivity extends AppCompatActivity {
+
+    Button add;
+    Button remove;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_homepage);
+
+        add = findViewById(R.id.addTopic);
+        remove = findViewById(R.id.minusTopic);
+
+        remove.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(HomepageActivity.this, "TOPIC REMOVE", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        add.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(HomepageActivity.this, "TOPIC ADDED", Toast.LENGTH_SHORT).show();
+            }
+        });
+
     }
 }
